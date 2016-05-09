e7af9bfa2e548952be8c66226bfd9ad5588ca401
kingdorian <doriandekoning@gmail.com>
2016-05-09 14:31:10 +0200

    Implement writing data to file

--- ECG-Logboek/app/src/main/java/com/kingdorian/android/ecg_logboek/AcitvityData.java
+++ ECG-Logboek/app/src/main/java/com/kingdorian/android/ecg_logboek/AcitvityData.java
@@ -1,8 +1,16 @@
 package com.kingdorian.android.ecg_logboek;
 
+import android.content.Context;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
@@ -10,25 +18,57 @@ import java.util.ArrayList;
  */
 public class AcitvityData {
 
-    private static ArrayList<HourEntry> data = new ArrayList<HourEntry>();
+    private ArrayList<HourEntry> data = new ArrayList<HourEntry>();
+
+    String FILENAME = "activityData";
 
 
-    public static ArrayList<HourEntry> getData() {
+
+    public ArrayList<HourEntry> getData() {
         return data;
     }
 
-    public static void addHourEntry(HourEntry hr) {
+    public void addHourEntry(HourEntry hr) {
         data.add(hr);
     }
 
-    public static void writeData() {
+    public void writeData(Context ctx) {
+
+        //TODO Write to file http://developer.android.com/guide/topics/data/data-storage.html
+        try {
+            FileOutputStream fos = ctx.openFileOutput(FILENAME, ctx.MODE_PRIVATE);
+            fos.write(getDataJSON().getBytes());
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readData(Context ctx) {
+        try {
+            FileInputStream fis = ctx.openFileInput(FILENAME);
+            InputStreamReader isr = new InputStreamReader();
+            BufferedReader buffered = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while((line = buffered.readLine()) != null) {
+                sb.append(line);
+            }
+            buffered.close();
+            isr.close();
+            System.out.println(sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getDataJSON() {
         JSONArray dataArray = new JSONArray();
-        JSONObject jsonObj = new JSONObject();
         for(HourEntry entry : data) {
             dataArray.put(entry.toJSON());
         }
-        //TODO Write to file http://developer.android.com/guide/topics/data/data-storage.html
         System.out.println(dataArray.toString());
+        return dataArray.toString();
     }
 
 }
