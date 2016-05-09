0dc6d437c782a94c66dbece59748547221642dc2
kingdorian <doriandekoning@gmail.com>
2016-05-09 14:13:03 +0200

    Add Activitydata class

---
+++ ECG-Logboek/app/src/main/java/com/kingdorian/android/ecg_logboek/AcitvityData.java
@@ -0,0 +1,34 @@
+package com.kingdorian.android.ecg_logboek;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by dorian on 9-5-16.
+ */
+public class AcitvityData {
+
+    private static ArrayList<HourEntry> data = new ArrayList<HourEntry>();
+
+
+    public static ArrayList<HourEntry> getData() {
+        return data;
+    }
+
+    public static addHourEntry(HourEntry hr) {
+        data.add(hr);
+    }
+
+    public static writeData() {
+        JSONArray dataArray = new JSONArray();
+        JSONObject jsonObj = new JSONObject();
+        for(HourEntry entry : data) {
+            dataArray.put(entry.toJSON());
+        }
+        //TODO Write to file http://developer.android.com/guide/topics/data/data-storage.html
+        System.out.println(dataArray.toString());
+    }
+
+}
